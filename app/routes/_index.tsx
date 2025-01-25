0d83d23aedcf07c2d690bb418f6b86801fd6f9e5
Laura Arcidiacono <laura.arcidiacono@softonic.com>
2025-01-25 19:26:28 +0100

    add index and destroy route

---
+++ app/routes/_index.tsx
@@ -0,0 +1,16 @@
+export default function Index() {
+    return (
+        <>
+            <p id="index-page"></p>
+            <div id="index-text">
+                <p>
+                    This is a Remix Contact List Manager.
+                </p>
+                <p>
+                    Check out{" "}
+                    <a href="https://github.com/LauraArcidiacono">my GitHub account</a>.
+                </p>
+            </div>
+        </>
+    );
+  }
